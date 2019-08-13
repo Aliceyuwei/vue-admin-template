03cf42b9f68658c0fbab785628377d39a128034a
alice <ke590pjmj7892@gmail.com>
2019-08-13 18:04:34 +0800

    dynamic-table - fixed component-update

--- src/views/table/dynamic-table/components/FixedThead.vue
+++ src/views/table/dynamic-table/components/FixedThead.vue
@@ -1,87 +1,69 @@
 <template>
   <div class="app-container">
-    <!-- find its imp but fail -->
     <div class="filter-container">
-      <el-checkbox-group v-model="formhead">
-        <el-checkbox label="country" />
-        <el-checkbox label="price" />
-        <el-checkbox label="product" />
-        <el-checkbox label="important" />
+      <span>order actually is your checkbox's order</span>
+      <!-- 所謂的order actually is your checkbox's order -->
+      <el-checkbox-group v-model="checkboxVal">
+        <el-checkbox label="onin">onin</el-checkbox>
+        <el-checkbox label="apple">apple</el-checkbox>
+        <el-checkbox label="banana">banana</el-checkbox>
+        <el-checkbox label="orange">orange</el-checkbox>
       </el-checkbox-group>
     </div>
-    <el-table :key="key" :data="tableData" border style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180" />
-      <el-table-column v-for="items in formhead" :key="items" :label="items">
-        <template slot-scope="{row}">
-          <span>{{ row[items] }}</span>
-        </template>
+
+    <el-table :key="key" :data="tableData" border fit highlight-current-row style="width: 100%">
+      <el-table-column prop="name" label="fruitName" width="180" />
+      <el-table-column v-for="fruit in formThead" :key="fruit" :label="fruit">
+        <template slot-scope="scope">{{ scope.row[fruit] }}</template>
       </el-table-column>
     </el-table>
   </div>
 </template>
+
 <script>
-const defaultFormThead = ['country', 'product']
+const defaultFormThead = ['apple', 'banana']
+
 export default {
   data() {
     return {
-      defaultFormThead: ['country', 'price', 'product', 'important'], // all option
-      /* defaultFormThead*/
-      formhead: defaultFormThead, // use to render table-column
-      checkboxVal: defaultFormThead, // USE TO HOOK WATCH
-      // In order to watch defaultFormThead , I divice 'defaultFormThead' to two key
-      // formhead&checkboxVal都指向同一個值defaultFormThead 所以我可以 用formhead來渲染col, 用checkboxVal來watch
-      /* defaultFormThead*/
-      key: 1, // table key
       tableData: [
         {
-          date: '2016-05-02',
-          country: 'EU',
-          price: 10000,
-          product: 'healthy food',
-          important: 5
+          name: 'fruit-1',
+          onin: 'onin-1',
+          apple: 'apple-10',
+          banana: 'banana-10',
+          orange: 'orange-10'
         },
         {
-          date: '2016-05-04',
-          country: 'CN',
-          price: 30000,
-          product: 'phone',
-          important: 3
-        },
-        {
-          date: '2016-05-01',
-          country: 'US',
-          price: 20000,
-          product: '$$$$$$',
-          important: 2
-        },
-        {
-          date: '2016-05-03',
-          country: 'JP',
-          price: 50000,
-          product: 'porn',
-          important: 1
-        },
-        {
-          date: '2016-05-03',
-          country: 'FR',
-          price: 40000,
-          product: 'culture items',
-          important: 4
+          name: 'fruit-2',
+          onin: 'onin-2',
+          apple: 'apple-20',
+          banana: 'banana-20',
+          orange: 'orange-20'
         }
-      ]
+      ],
+      key: 1, // table key
+      formTheadOptions: ['onin', 'apple', 'banana', 'orange'],
+      checkboxVal: defaultFormThead, // checkboxVal
+      formThead: defaultFormThead // 默认表头 Default header
     }
   },
   watch: {
     checkboxVal(valArr) {
+      console.log('我變了')
+      //arr.filter方法 => 過濾出 arr裡 >= 0 的取出
       this.formThead = this.formTheadOptions.filter(
-        i => valArr.indexOf(i) >= 0
+        i => {
+          console.log(valArr.indexOf(i), 'valArr.indexOf(i) ')
+          return valArr.indexOf(i) >= 0
+
+          // console.log(i, 'i')
+        }
         // 好像是利用indexOf()但段此樹是否存在，在依照你原本arrformTheadOptions的順序
       )
-      this.key = this.key + 1 // 为了保证table 每次都会重渲 In order to ensure the table will be re-rendered each time
+      // this.key = this.key + 1 // 为了保证table 每次都会重渲 In order to ensure the table will be re-rendered each time
     }
   }
 }
 </script>
-<style>
-</style>
 
