6f8957f83c19473ac9c83de8c33362a30c69985f
alice <ke590pjmj7892@gmail.com>
2019-08-14 10:54:10 +0800

    install vuedraggable-- use vuedraggable instead sortable.js

--- src/views/components/drag/drag-1/index.vue
+++ src/views/components/drag/drag-1/index.vue
@@ -1,61 +1,12 @@
-<!-- https://sortablejs.github.io/Sortable/ -->
-<!-- 我希望是item -->
+<!-- threr used vuedraggable not  Sortable.js -->
+<!--BIG WORD WORDS*** vuedraggable based on Sortable.js ***BIG WORD WORDS-->
 <template>
-  <div class="app-container">
-    <ul>
-      <li v-for="list in list" ref="drag" :key="list.id">{{ list.title }}</li>
-    </ul>
-  </div>
+  <div class="app-container"/>
 </template>
 
 <script>
-import { getList } from '@/api/table';
-import Sortable from 'sortablejs';
-export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
-  data() {
-    return {
-      list: null,
-      listLoading: true
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData() {
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
-      this.$nextTick(() => {
-        this.sort()
-      })
-    },
-    sort() {
-      console.log('123456')
-      console.log('this:', this.$refs)
-    }
-  }
-}
+export default {}
 </script>
 <style scoped>
-/* *li {
-  border: 1px solid red;
-} */
-.app-container > ul > li {
-  border: 1px solid #42b983;
-  padding: 10px 10px;
-}
 </style>
 
