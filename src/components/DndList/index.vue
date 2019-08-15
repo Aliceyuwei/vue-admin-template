0d28f3926df065a5e2af21477418ea7fa2abbcf0
alice <ke590pjmj7892@gmail.com>
2019-08-15 16:54:06 +0800

    update 將drag拆成分父子組件

--- src/components/DndList/index.vue
+++ src/components/DndList/index.vue
@@ -1,43 +1,100 @@
 <template>
-  <div class="board-column">
-    <div class="board-column-header">{{ headerText }}</div>
-    <draggable :list="list" v-bind="$attrs" class="board-column-content" :set-data="setData">
-      <div
-        v-for="element in list"
-        :key="element.id"
-        class="board-item"
-      >{{ element.name }} {{ element.id }}</div>
-    </draggable>
+  <div class="dndList">
+    <div :style="{width:width1}" class="dndList-list">
+      <h3>{{ list1Title }}</h3>
+      <draggable :set-data="setData" :list="list1" group="article" class="dragArea">
+        <div v-for="element in list1" :key="element.id" class="list-complete-item">
+          <div class="list-complete-item-handle">
+            {{ element.id }}[{{ element.author }}] {{ element.title }}
+          </div>
+          <div style="position:absolute;right:0px;">
+            <span style="float: right ;margin-top: -20px;margin-right:5px;" @click="deleteEle(element)">
+              <i style="color:#ff4949" class="el-icon-delete" />
+            </span>
+          </div>
+        </div>
+      </draggable>
+    </div>
+    <div :style="{width:width2}" class="dndList-list">
+      <h3>{{ list2Title }}</h3>
+      <draggable :list="list2" group="article" class="dragArea">
+        <div v-for="element in list2" :key="element.id" class="list-complete-item">
+          <div class="list-complete-item-handle2" @click="pushEle(element)">
+            {{ element.id }} [{{ element.author }}] {{ element.title }}
+          </div>
+        </div>
+      </draggable>
+    </div>
   </div>
 </template>
 
 <script>
-import draggable from 'vuedraggable';
+import draggable from 'vuedraggable'
 
 export default {
-  name: 'DragKanbanDemo',
-  components: {
-    draggable
-  },
+  name: 'DndList',
+  components: { draggable },
   props: {
-    headerText: {
-      type: String,
-      default: 'Header'
-    },
-    options: {
-      type: Object,
-      default() {
-        return {}
-      }
-    },
-    list: {
+    list1: {
       type: Array,
       default() {
         return []
       }
+    },
+    list2: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    list1Title: {
+      type: String,
+      default: 'list1'
+    },
+    list2Title: {
+      type: String,
+      default: 'list2'
+    },
+    width1: {
+      type: String,
+      default: '48%'
+    },
+    width2: {
+      type: String,
+      default: '48%'
     }
   },
   methods: {
+    isNotInList1(v) {
+      return this.list1.every(k => v.id !== k.id)
+    },
+    isNotInList2(v) {
+      return this.list2.every(k => v.id !== k.id)
+    },
+    deleteEle(ele) {
+      for (const item of this.list1) {
+        if (item.id === ele.id) {
+          const index = this.list1.indexOf(item)
+          this.list1.splice(index, 1)
+          break
+        }
+      }
+      if (this.isNotInList2(ele)) {
+        this.list2.unshift(ele)
+      }
+    },
+    pushEle(ele) {
+      for (const item of this.list2) {
+        if (item.id === ele.id) {
+          const index = this.list2.indexOf(item)
+          this.list2.splice(index, 1)
+          break
+        }
+      }
+      if (this.isNotInList1(ele)) {
+        this.list1.push(ele)
+      }
+    },
     setData(dataTransfer) {
       // to avoid Firefox bug
       // Detail see : https://github.com/RubaXa/Sortable/issues/1012
@@ -46,49 +103,64 @@ export default {
   }
 }
 </script>
+
 <style lang="scss" scoped>
-.board-column {
-  min-width: 300px;
-  min-height: 100px;
-  height: auto;
-  overflow: hidden;
-  background: #f0f0f0;
-  border-radius: 3px;
-
-  .board-column-header {
-    height: 50px;
-    line-height: 50px;
-    overflow: hidden;
-    padding: 0 20px;
-    text-align: center;
-    background: #333;
-    color: #fff;
-    border-radius: 3px 3px 0 0;
+.dndList {
+  background: #fff;
+  padding-bottom: 40px;
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
   }
-
-  .board-column-content {
-    height: auto;
-    overflow: hidden;
-    border: 10px solid transparent;
-    min-height: 60px;
-    display: flex;
-    justify-content: flex-start;
-    flex-direction: column;
-    align-items: center;
-
-    .board-item {
-      cursor: pointer;
-      width: 100%;
-      height: 64px;
-      margin: 5px 0;
-      background-color: #fff;
-      text-align: left;
-      line-height: 54px;
-      padding: 5px 10px;
-      box-sizing: border-box;
-      box-shadow: 0px 1px 3px 0 rgba(0, 0, 0, 0.2);
+  .dndList-list {
+    float: left;
+    padding-bottom: 30px;
+    &:first-of-type {
+      margin-right: 2%;
+    }
+    .dragArea {
+      margin-top: 15px;
+      min-height: 50px;
+      padding-bottom: 30px;
     }
   }
 }
-</style>
 
+.list-complete-item {
+  cursor: pointer;
+  position: relative;
+  font-size: 14px;
+  padding: 5px 12px;
+  margin-top: 4px;
+  border: 1px solid #bfcbd9;
+  transition: all 1s;
+}
+
+.list-complete-item-handle {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-right: 50px;
+}
+
+.list-complete-item-handle2 {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-right: 20px;
+}
+
+.list-complete-item.sortable-chosen {
+  background: #4AB7BD;
+}
+
+.list-complete-item.sortable-ghost {
+  background: #30B08F;
+}
+
+.list-complete-enter,
+.list-complete-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ src/views/components-demo/drag/drag-4/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="components-container">
+    <aside>
+      drag-list base on
+      <a
+        href="https://github.com/SortableJS/Vue.Draggable"
+        target="_blank"
+      >Vue.Draggable</a>
+    </aside>
+    <div class="editor-container">
+      <dnd-list :list1="list1" :list2="list2" list1-title="List" list2-title="Article pool" />
+    </div>
+  </div>
+</template>
+
+<script>
+import DndList from '@/components/DndList';
+
+export default {
+  name: 'DndListDemo',
+  components: { DndList },
+  data() {
+    return {
+      list1: [],
+      list2: [],
+      items: [
+        {
+          id: 0,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 1,
+          author: 'paris',
+          title: 'ali11111111111111111111111111111111111111111111'
+        },
+        {
+          id: 2,
+          author: 'paris',
+          title: 'ali222222222222222222222222222222222222222222222'
+        },
+        {
+          id: 3,
+          author: 'paris',
+          title: 'ali3333333333333333333333333333333333'
+        },
+        {
+          id: 4,
+          author: 'paris',
+          title: 'ali444444444444444444444444444444444444444444444'
+        },
+        {
+          id: 5,
+          author: 'paris',
+          title: 'ali555555555555555555555555555555555555555555555'
+        },
+        { id: 6, author: 'paris', title: 'ali666666666666666666' },
+        {
+          id: 7,
+          author: 'paris',
+          title: 'ali777777777777777777777777777777777777777777777'
+        },
+        {
+          id: 8,
+          author: 'paris',
+          title: 'ali8888888888888888888888888888888888888888888'
+        },
+        {
+          id: 9,
+          author: 'paris',
+          title: 'ali9999999999999999999999999999999999999999999999'
+        },
+        { id: 10, author: 'paris', title: 'ali10101010101010101010' },
+        {
+          id: 11,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 12,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 13,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 14,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 15,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 16,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 17,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 18,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 19,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        },
+        {
+          id: 20,
+          author: 'paris',
+          title: 'ali00000000000000000000000000000000000000000000'
+        }
+      ]
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    getData() {
+      this.list1 = this.items.splice(0, 5)
+      this.list2 = this.items
+    }
+  }
+}
+</script>
+
