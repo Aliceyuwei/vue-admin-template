83622aeac0a2d9886fe773f1e3632bddf6b2c286
alice <ke590pjmj7892@gmail.com>
2019-08-15 16:38:11 +0800

    改router名稱和結構

--- src/router/modules/components.js
+++ src/router/modules/components.js
@@ -4,51 +4,51 @@ const componentsRouter = {
   path: '/components',
   component: Layout,
   redirect: 'noRedirect',
-  name: 'Components',
+  name: 'ComponentDemo',
   meta: {
     title: 'Components',
-    icon: 'nested'
+    icon: 'component'
   },
   children: [
     {
       path: 'drag',
-      component: () => import('@/views/components/drag/index'), // Parent router-view
+      component: () => import('@/views/components-demo/drag/index'), // Parent router-view
       name: 'Drag',
       meta: { title: 'Drag' },
       children: [
         {
           path: 'drag-1',
-          component: () => import('@/views/components/drag/drag-1'),
+          component: () => import('@/views/components-demo/drag/drag-1'),
           name: 'Drag-1',
           meta: { title: 'Drag-1' }
         },
         {
           path: 'drag-2',
-          component: () => import('@/views/components/drag/drag-2'),
+          component: () => import('@/views/components-demo/drag/drag-2'),
           name: 'Drag-2',
           meta: { title: 'Drag-2' }
         },
         {
           path: 'drag-3',
-          component: () => import('@/views/components/drag/drag-3'),
+          component: () => import('@/views/components-demo/drag/drag-3'),
           name: 'Drag-3',
           meta: { title: 'Drag-3' }
         },
         {
           path: 'drag-4',
-          component: () => import('@/views/components/drag/drag-4'),
+          component: () => import('@/views/components-demo/drag/drag-4'),
           name: 'Drag-4',
           meta: { title: 'Drag-4' }
         },
         {
           path: 'drag-5',
-          component: () => import('@/views/components/drag/drag-5'),
+          component: () => import('@/views/components-demo/drag/drag-5'),
           name: 'Drag-5',
           meta: { title: 'Drag-5' }
         },
         {
           path: 'drag-6',
-          component: () => import('@/views/components/drag/drag-6'),
+          component: () => import('@/views/components-demo/drag/drag-6'),
           name: 'Drag-6',
           meta: { title: 'Drag-6' }
         }
@@ -56,8 +56,14 @@ const componentsRouter = {
     },
     {
       path: 'menu2',
-      component: () => import('@/views/components/menu2/index'),
+      component: () => import('@/views/components-demo/menu2/index'),
       meta: { title: 'menu2' }
+    },
+    {
+      path: 'drag-kanban',
+      component: () => import('@/views/components-demo/drag-kanban'),
+      name: 'DragKanbanDemo',
+      meta: { title: 'Drag Kanban' }
     }
   ]
 }
